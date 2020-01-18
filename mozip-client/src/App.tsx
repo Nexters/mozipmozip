9551d62fe7a78896e935d9414fa3b8a06444641f
tramyu <tramyu@naver.com>
2020-01-18 21:12:45 +0900

    [#15] feat - integration react app and spring, refactor directories
    - 리액트 앱을 묶어서 배포할 수 있도록 그래들 설정 추가
    - 디렉토리 구조 변경(mozip-server 디렉토리 하위로 이동)
    - skaffold context 변경
    - ingress path /*로 변경 (static file 못찾는 문제 해결) - https://stackoverflow.com/questions/48394686/kubernetes-ingress-service-can-not-load-static-files
    - serialize-javascript 의존성 취약점 조치

--- mozip-client/src/App.tsx
+++ mozip-client/src/App.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 const App: React.FC = () => {
     return (
         <div className="App">
+            <>
+                모집모집모집모집
+            </>
         </div>
     );
 };
