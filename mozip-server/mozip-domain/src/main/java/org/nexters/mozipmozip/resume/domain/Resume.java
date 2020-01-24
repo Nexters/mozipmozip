34d039ebb3f30d62dd850180f678be742f57df5c
janghyoseog <bestdevhyo1225@gmail.com>
2020-01-24 18:36:33 +0900

    [#25] feat: Add user column in resume model
    - Resume 모델에 외래키인 User 추가

--- mozip-server/mozip-domain/src/main/java/org/nexters/mozipmozip/resume/domain/Resume.java
+++ mozip-server/mozip-domain/src/main/java/org/nexters/mozipmozip/resume/domain/Resume.java
@@ -2,6 +2,7 @@ package org.nexters.mozipmozip.resume.domain;
 
 import lombok.*;
 import org.nexters.mozipmozip.JpaBasePersistable;
+import org.nexters.mozipmozip.member.domain.User;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -11,10 +12,14 @@ import java.util.List;
 @Setter
 @Getter
 @Entity
-@Table(name = "resume")
+@Table(name = "resume", indexes = { @Index(columnList = "user_id", name = "IDX_USER_ID")})
 @AttributeOverride(name = "id", column = @Column(name = "resume_id"))
 @NoArgsConstructor(access = AccessLevel.PACKAGE)
 public class Resume extends JpaBasePersistable {
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "occupation", nullable = false)
