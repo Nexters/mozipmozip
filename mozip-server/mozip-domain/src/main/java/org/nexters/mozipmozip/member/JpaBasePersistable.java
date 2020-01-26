cea0967b5a8084f9c7b26a0ce3aae03e6ab674d3
rayoungji <dudwl7676@naver.com>
2020-01-26 19:13:37 +0900

    [26] 프로젝트 다시 클론  후 충돌 해결

---
+++ mozip-server/mozip-domain/src/main/java/org/nexters/mozipmozip/member/JpaBasePersistable.java
@@ -0,0 +1,24 @@
+package org.nexters.mozipmozip.member;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import javax.persistence.*;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@EqualsAndHashCode(of = "id", callSuper = false)
+@Getter
+public abstract class JpaBasePersistable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    @Column(name = "deleted", nullable = false, columnDefinition = "BIT default 0")
+    protected Boolean deleted = false;
+
+    public void delete() {
+        deleted = true;
+    }
+}
+
