6eea9b01df2658e4e37018b7497aeb3c0d0be2b0
rayoungji <dudwl7676@naver.com>
2020-01-27 20:33:10 +0900

    [26] 회원 api&service 정의 -servicetest 구현중

--- mozip-server/mozip-api/src/main/java/org/nexters/mozipmozip/member/controller/UserController.java
+++ mozip-server/mozip-api/src/main/java/org/nexters/mozipmozip/member/controller/UserController.java
@@ -2,16 +2,19 @@ package org.nexters.mozipmozip.member.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.nexters.mozipmozip.member.application.UserService;
+import org.nexters.mozipmozip.member.domain.User;
 import org.nexters.mozipmozip.member.dto.UserCreateDto;
+import org.nexters.mozipmozip.member.dto.UserGetDto;
+import org.nexters.mozipmozip.member.dto.UserLoginDto;
+import org.nexters.mozipmozip.member.dto.UserUpdateDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.net.URI;
 
@@ -21,7 +24,6 @@ import java.net.URI;
 public class UserController {
 
     final private UserService userService;
-
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
     //회원가입 api
@@ -39,8 +41,65 @@ public class UserController {
         }
 
         return ResponseEntity.created(URI.create("users"))
-                .body(userService.createUser(UserCreateDto.of(userCreateDto)));
+                .body(userService.createUser(UserCreateDto.toEntity(userCreateDto)));
     }
 
+    //로그인 api
+    @PostMapping(value = "/login")
+    public String loginUser(@RequestBody UserLoginDto userLoginDto, HttpServletRequest request) {
+        //리턴받은 회원정보로 세션 생성 후 회원정보 저장하기
+        User user = userService.signInUser(UserLoginDto.toEntity(userLoginDto));
+        HttpSession httpSession = request.getSession(true);
+        httpSession.setAttribute("userInfo", user);
+        User userInfo = (User) httpSession.getAttribute("userInfo");
+        log.info(userInfo.getName());
+        log.info(userInfo.getEmail());
+        final String isLogin = "로그인 성공!!!";
+        return isLogin;
+    }
+
+    //로그아웃 api
+    @PostMapping(value = "/logout")
+    public String logoutUser(HttpServletRequest request) {
+        request.getSession();
+        //세션 초기화
+        request.getSession().invalidate();
+        request.getSession();
+        return userService.logoutUser();
+    }
+
+    //회원조회 api
+    @GetMapping(value = "/getUser")
+    public UserGetDto getUser(HttpSession session) {
+        User userInfo = userService.getUser(session);
+        //세션에 저장되어있던 회원정보 dto로 변환하여 응답하기
+        UserGetDto userGetDto = UserGetDto.builder()
+                .name(userInfo.getName())
+                .email(userInfo.getEmail())
+                .phoneNumber(userInfo.getPhoneNumber())
+                .isAdmin(userInfo.getIsAdmin())
+                .build();
+        return userGetDto;
+    }
+
+    //회원정보 수정(수정된 회원정보(비밀번호,이메일,전화번호)와 기존 session 요청) api
+    @PutMapping(value = "/updateUser")
+    public UserUpdateDto updateUser(HttpSession session, @RequestBody UserUpdateDto userUpdateDto) {
+        User updateInfo = userService.updateUser(session, UserUpdateDto.toEntity(userUpdateDto));
+        UserUpdateDto updateDto = UserUpdateDto.builder()
+                .email(updateInfo.getEmail())
+                .password(updateInfo.getPassword())
+                .phoneNumber(updateInfo.getPhoneNumber()).build();
+        return updateDto;
+    }
+
+    //회원탈퇴 api
+    @DeleteMapping(value = "/deleteUser")
+    public String deleteUser(HttpSession session) {
+        session.invalidate();
+        userService.deleteUser(session);
+        String delete = "탈퇴하였습니다";
+        return delete;
+    }
 }
 
