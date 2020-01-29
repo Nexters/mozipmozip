69f9f67bc0fbf8272037d642488dc82c2b135d4c
doonguk <mbxd1@naver.com>
2020-01-29 11:35:22 +0900

    [#36] feat - admin header base layout

--- mozip-client/src/components/common/Admin/Header/Header.tsx
+++ mozip-client/src/components/common/Admin/Header/Header.tsx
@@ -3,10 +3,21 @@ import *as Styled from './style'
 
 function Header(){
   return(
-    <Styled.Header>
-      <Styled.title>모집모집</Styled.title>
-
-    </Styled.Header>
+      <Styled.Header>
+        <Styled.Half>
+          <Styled.Title>모집모집</Styled.Title>
+          <Styled.Category>
+            <Styled.CategoryItem
+              style={{paddingRight : '5%'}}
+            >진행중인 리쿠르팅</Styled.CategoryItem>
+            <Styled.CategoryItem>리쿠르팅 목록</Styled.CategoryItem>
+          </Styled.Category>
+        </Styled.Half>
+        <Styled.Half>
+          <Styled.Name>홍동욱</Styled.Name>
+          <Styled.Profile/>
+        </Styled.Half>
+      </Styled.Header>
   )
 }
 
