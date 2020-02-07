26b872c8eef1a4cfddaa2daf0a121017bd80e586
hzoou <woojooo52@naver.com>
2020-02-08 01:36:50 +0900

    feat: 회원가입 페이지 UI 구현
    회원가입 페이지의 전체적인 UI를 구현했습니다.
    적용되어있는 기능은 인풋에 포커싱이 된 경우
    테두리의 색상이 변경되는 정도의 기능만 반영된 상태이며 추후 추가할 예정입니다.
    브라우저의 크기가 작은 경우에 대응이 아직 부족하므로 해당 사항도 반영할 예정입니다.

--- mozip-client/src/App.tsx
+++ mozip-client/src/App.tsx
@@ -4,12 +4,14 @@ import {
   MainPage,
   AdminPage,
   NotFoundPage,
+  SignUpPage,
   TodoPage, EmployPage} from "./pages"
 
 export default function App() {
   return(
     <Switch>
       <Route exact path={'/'} component={MainPage}/>
+      <Route exact path={'/signup'} component={SignUpPage}/>
       <Route path={'/admin/:path?/:subPath?'} component={AdminPage}/>
 
       <Route path={'/todo'} component={TodoPage}/>
---
+++ mozip-client/src/containers/SignUp/SignUpContainer/style.ts
@@ -0,0 +1,82 @@
+import styled from 'styled-components';
+
+interface ITabProps {
+  clicked: boolean
+}
+
+const Main = styled.main`
+  width: 100vw;
+  height: calc(100% - 70px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #F7F8F9;
+`;
+
+const Container = styled.section`
+  display: flex;
+  justify-content: space-between;
+  width: 886px;
+  background: #FFFFFF;
+  border: 1px solid #D0D3D8;
+  border-radius: 8px;
+  padding: 52px 58px 64px 54px;
+`;
+
+const LeftWrapper = styled.article`
+
+`;
+
+const TabContainer = styled.section`
+  margin-bottom: 48px;
+`;
+
+const Tab = styled.article<ITabProps>`
+  margin-right: 40px;
+  display: inline-flex;
+  padding-bottom: 7px;
+  font-size: 20px;
+  line-height: 30px;
+  color: ${({ clicked }) => clicked? '#61CB9F' : '#6D7176'};
+  border-bottom: ${({ clicked }) => clicked && '2px solid #61CB9F'};
+  cursor: pointer;
+`;
+
+//TODO 버튼 컴포넌트화
+const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 47px;
+  width: 317px;
+  height: 70px;
+  background: #61CB9F;
+  border-radius: 8px;
+  font-family: Roboto;
+  font-size: 20px;
+  color: #FFFFFF;
+`;
+
+const RightWrapper = styled.article`
+  display: flex;
+  align-items: center;
+`;
+
+//TODO 이미지로 변경
+const Circle = styled.div`
+  width: 339px;
+  height: 339px;
+  background: #F4F4F4;
+  border-radius: 50%;
+`;
+
+export {
+  Main,
+  Container,
+  LeftWrapper,
+  TabContainer,
+  Tab,
+  Button,
+  RightWrapper,
+  Circle
+}
