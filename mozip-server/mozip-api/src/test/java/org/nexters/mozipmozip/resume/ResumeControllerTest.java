207b20b27eeccdc21c55429a0f7bc7c24f2475db
janghyoseog <bestdevhyo1225@gmail.com>
2020-02-08 20:28:12 +0900

    [#54] test: Add controller test case
    - 컨트롤러 테스트 케이스 추가

--- mozip-server/mozip-api/src/test/java/org/nexters/mozipmozip/resume/ResumeControllerTest.java
+++ mozip-server/mozip-api/src/test/java/org/nexters/mozipmozip/resume/ResumeControllerTest.java
@@ -13,21 +13,21 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.nexters.mozipmozip.resume.application.ResumeService;
 import org.nexters.mozipmozip.resume.controller.ResumeController;
 import org.nexters.mozipmozip.resume.domain.Resume;
+import org.nexters.mozipmozip.resume.domain.ResumeAnswerItem;
 import org.nexters.mozipmozip.resume.dto.ResumeCreateDto;
 import org.nexters.mozipmozip.resume.dto.ResumeUpdateDto;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collections;
 
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
@@ -45,6 +45,7 @@ public class ResumeControllerTest {
     private MockMvc mockMvc;
     private ResumeCreateDto resumeCreateDto = new EasyRandom().nextObject(ResumeCreateDto.class);
     private ResumeUpdateDto resumeUpdateDto = new EasyRandom().nextObject(ResumeUpdateDto.class);
+    private Resume resumeFixture = new EasyRandom().nextObject(Resume.class);
 
     @BeforeEach
     public void setUp() {
@@ -56,29 +57,88 @@ public class ResumeControllerTest {
     @Test
     @DisplayName("지원서 생성 API 테스트")
     void createResume() throws Exception {
-        Resume resumeFixture = resumeCreateDto.of();
+        Resume resumeCreateDtoFixture = resumeCreateDto.of();
 
-        given(resumeService.save(resumeFixture)).willReturn(resumeFixture);
+        given(resumeService.save(resumeCreateDtoFixture)).willReturn(this.resumeFixture);
+
+        mockMvc.perform(post("/api/v1/resumes")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(this.objectMapper.writeValueAsString(resumeCreateDto)))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    @DisplayName("지원서 생성시 잘못된 파라미터가 들어오면 Bad Request로 리턴한다.")
+    void createResumeInvalidParameter() throws Exception {
+        this.resumeCreateDto.setEmail("");
+
+        mockMvc.perform(post("/api/v1/resumes")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(this.objectMapper.writeValueAsString(resumeCreateDto)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("모든 지원서 조회 API 테스트")
+    void getAll() throws Exception {
+        given(resumeService.getResumes()).willReturn(Collections.singletonList(this.resumeFixture));
+
+        mockMvc.perform(get("/api/v1/resumes")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].state").exists())
+                .andExpect(jsonPath("$[0].occupation").exists())
+                .andExpect(jsonPath("$[0].resumeJobType").exists());
+    }
+
+    @Test
+    @DisplayName("resumeId로 특정 지원서 조회 API 테스트")
+    void getById() throws Exception {
+        Long resumeIdFixture = 1L;
+        ResumeAnswerItem resumeAnswerItemFixture = new EasyRandom().nextObject(ResumeAnswerItem.class);
+        resumeAnswerItemFixture.setResume(this.resumeFixture);
+        this.resumeFixture.setResumeAnswerItems(Collections.singletonList(resumeAnswerItemFixture));
+
+        given(resumeService.getResumeById(resumeIdFixture)).willReturn(this.resumeFixture);
+
+        mockMvc.perform(get("/api/v1/resumes/" + resumeIdFixture)
+                    .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value(this.resumeFixture.getName()))
+                .andExpect(jsonPath("$.email").value(this.resumeFixture.getEmail()))
+                .andExpect(jsonPath("$.phoneNumber").value(this.resumeFixture.getPhoneNumber()))
+                .andExpect(jsonPath("$.blogURL").value(this.resumeFixture.getBlogURL()))
+                .andExpect(jsonPath("$.githubURL").value(this.resumeFixture.getGithubURL()))
+                .andExpect(jsonPath("$.portFolioURL").value(this.resumeFixture.getPortFolioURL()))
+                .andExpect(jsonPath("$.state").exists())
+                .andExpect(jsonPath("$.occupation").exists())
+                .andExpect(jsonPath("$.resumeJobType").exists())
+                .andExpect(jsonPath("$.resumeAnswerItems").exists());
+    }
+
+    @Test
+    @DisplayName("noticeId로 모든 지원서 조회 API 테스트")
+    void getAllByNoticeId() throws Exception {
+        Long noticeFixtureId = 1L;
+
+        given(resumeService.getResumesByNoticeId(noticeFixtureId)).willReturn(Collections.singletonList(this.resumeFixture));
 
         mockMvc.perform(
-                post("/api/v1/resumes")
+                get("/api/v1/resumes/notices/{noticeId}", noticeFixtureId)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(this.objectMapper.writeValueAsString(resumeCreateDto))
         ).andExpect(status().isOk());
     }
 
     @Test
-    @DisplayName("특정 지원서 조회 API 테스트")
-    void getResumeById() throws Exception {
-        Resume resumeFixture = resumeCreateDto.of();
-        Long idFixture = 1L;
+    @DisplayName("userId로 모든 지원서 조회 API 테스트")
+    void getAllByUserId() throws Exception {
+        Long userFixtureId = 1L;
 
-        given(resumeService.getById(idFixture)).willReturn(resumeFixture);
+        given(resumeService.getResumesByUserId(userFixtureId)).willReturn(Collections.singletonList(this.resumeFixture));
 
         mockMvc.perform(
-                get("/api/v1/resumes/" + idFixture)
+                get("/api/v1/resumes/users/{userId}", userFixtureId)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(this.objectMapper.writeValueAsString(resumeUpdateDto))
         ).andExpect(status().isOk());
     }
 
@@ -95,37 +155,4 @@ public class ResumeControllerTest {
                         .content(this.objectMapper.writeValueAsString(resumeUpdateDto))
         ).andExpect(status().isOk());
     }
-
-    @Test
-    @DisplayName("모든 지원서 조회 API 테스트")
-    void getAllResume() throws Exception {
-        List<Resume> resumeListFixture = new ArrayList<>();
-
-        resumeListFixture.add(resumeCreateDto.of());
-        resumeListFixture.add(resumeCreateDto.of());
-
-        given(resumeService.getAll()).willReturn(resumeListFixture);
-
-        mockMvc.perform(
-                get("/api/v1/resumes")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(this.objectMapper.writeValueAsString(resumeListFixture))
-        ).andExpect(status().isOk());
-    }
-
-    @Test
-    @DisplayName("특정 지원서 삭제 API 테스트")
-    void deleteResume() throws Exception {
-        Resume resumeFixture = resumeCreateDto.of();
-        Long idFixture = 1L;
-
-        given(resumeService.delete(idFixture)).willReturn(resumeFixture);
-
-        mockMvc.perform(
-                delete("/api/v1/resumes/" + idFixture)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(this.objectMapper.writeValueAsString(resumeFixture))
-        ).andExpect(status().isOk());
-    }
-
 }
