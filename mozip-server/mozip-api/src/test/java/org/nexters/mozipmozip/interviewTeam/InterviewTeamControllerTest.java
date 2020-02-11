d56991cbe0827039395ccc5deff08d78423ed80e
rayoungji <dudwl7676@naver.com>
2020-02-12 00:48:27 +0900

    [#61]인터뷰팀조회 api 작성

--- mozip-server/mozip-api/src/test/java/org/nexters/mozipmozip/interviewTeam/InterviewTeamControllerTest.java
+++ mozip-server/mozip-api/src/test/java/org/nexters/mozipmozip/interviewTeam/InterviewTeamControllerTest.java
@@ -24,8 +24,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
@@ -79,5 +81,31 @@ class InterviewTeamControllerTest {
                         .content(objectMapper.writeValueAsString(createInterviewTeamDto))
         )
                 .andExpect(status().isCreated());
+    }
+
+    @Test
+    @DisplayName("인터뷰팀 조회")
+    void getInterviewTeam() throws Exception {
+        userId.add(1L);
+        userId.add(2L);
+        resumeId.add(0L);
+        resumeId.add(1L);
+        CreateInterviewTeamDto createInterviewTeamDto = CreateInterviewTeamDto.builder()
+                .title("A")
+                .usersIds(userId)
+                .resumesIds(resumeId)
+                .startDate(LocalDate.now())
+                .startTime(LocalTime.now())
+                .endTime(LocalTime.now()).build();
+
+        InterviewTeam createInterviewTeam = CreateInterviewTeamDto.toEntity(createInterviewTeamDto);
+        given(interviewTeamService.getInterviewTeam(interviewTeam.getId())).willReturn(interviewTeam);
+
+        mockMvc.perform(
+                get("/api/v1/interview/{id}", interviewTeam.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+        )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value(interviewTeam.getTitle()));
     }
 }
