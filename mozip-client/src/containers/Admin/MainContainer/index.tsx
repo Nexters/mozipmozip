fb58869c1fe0cf9e269bf1967b294fd8349701aa
hzoou <woojooo52@naver.com>
2020-02-15 21:27:59 +0900

    feat: #99 어드민 페이지 디자인

---
+++ mozip-client/src/containers/Admin/MainContainer/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import * as Styled from './styled';
+import { Link } from 'react-router-dom';
+import Button from '../../../components/common/Button';
+import banner from '../../../static/images/banner.png';
+
+function MainContainer() {
+  return (
+    <Styled.Container>
+      <Styled.Banner src={banner} />
+      <Styled.BottomSection>
+        <Link to={'/admin/create/intro'}>
+          <Button text={'리쿠르팅 등록하기'} />
+        </Link>
+      </Styled.BottomSection>
+    </Styled.Container>
+  );
+}
+
+export default MainContainer;
