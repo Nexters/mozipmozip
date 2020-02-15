e15e3c4ad18cc59cb80f81efb45c3ab5d4468832
tramyu <tramyu@naver.com>
2020-02-15 22:56:16 +0900

    [#00] refactor - add unique constraints

--- mozip-server/mozip-domain/src/main/java/org/nexters/mozipmozip/user/domain/User.java
+++ mozip-server/mozip-domain/src/main/java/org/nexters/mozipmozip/user/domain/User.java
@@ -24,7 +24,7 @@ public class User extends JpaBasePersistable {
     protected Boolean isAdmin = false;  //원래는 모두 지원자 인증번호받으면 관리자로 플래그값 변경
     @Column(name = "name", length = 30, nullable = false)
     private String name;
-    @Column(name = "email", length = 50, nullable = false)
+    @Column(name = "email", length = 50, nullable = false, unique = true)
     private String email;
     @Column(name = "password", length = 30, nullable = false)
     @JsonIgnore
