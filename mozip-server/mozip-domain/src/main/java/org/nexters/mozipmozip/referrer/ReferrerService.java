de5a937844f937246c5f2ababc819f63177ff954
tramyu <tramyu@naver.com>
2020-02-19 21:56:15 +0900

    [#00] feat - add referrer api for get

--- mozip-server/mozip-domain/src/main/java/org/nexters/mozipmozip/referrer/ReferrerService.java
+++ mozip-server/mozip-domain/src/main/java/org/nexters/mozipmozip/referrer/ReferrerService.java
@@ -2,13 +2,22 @@ package org.nexters.mozipmozip.referrer;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class ReferrerService {
     private final ReferrerRepository referrerRepository;
 
     public Referrer createReferrer(Referrer referrer) {
         return referrerRepository.save(referrer);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Referrer> getAllReferrers() {
+        return referrerRepository.findAll();
     }
 }
